134eb973e60e59c9f8a9f09a773860990e75dd3f
Laure <laure.milian@gmail.com>
2017-01-16 11:30:08 +0100

    creation des fonctions italic, underline et capitalize

--- index.php
+++ index.php
@@ -11,9 +11,24 @@ Class StrUtils {
 	public function bold() {
 		return '<strong>' . $this->_str . '</strong>';
 	}
+
+	public function italic() {
+		return '<i>' . $this->_str . '</i>';
+	}
+
+	public function underline() {
+		return '<u>' . $this->_str . '</u>';
+	}
+
+	public function capitalize() {
+		return ucfirst($this->_str);
+	}
 }
 
-$Obj = new StrUtils("Blabla");
+$Obj = new StrUtils("blabla");
 var_dump($Obj);
 echo $Obj->str;
 echo $Obj->bold();
+echo $Obj->italic();
+echo $Obj->underline();
+echo $Obj->capitalize();
