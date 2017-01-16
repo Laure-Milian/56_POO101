5a9dc5ef3080aa2547eb92a202639079b4d85391
Laure <laure.milian@gmail.com>
2017-01-16 11:21:03 +0100

    ajout methode bold

--- index.php
+++ index.php
@@ -7,8 +7,13 @@ Class StrUtils {
 	public function __construct($newString) {
 		$this->_str = $newString;
 	}
+
+	public function bold() {
+		return '<strong>' . $this->_str . '</strong>';
+	}
 }
 
 $Obj = new StrUtils("Blabla");
 var_dump($Obj);
 echo $Obj->str;
+echo $Obj->bold();
